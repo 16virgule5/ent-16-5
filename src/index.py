2ca87fb42074693ce721c8e37e9116d86b0aadcb
Clément <159389431+Clmnnt@users.noreply.github.com>
2025-02-16 00:06:31 +0100

    chore(index.py): add a beautiful comment

--- src/index.py
+++ src/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def schoolname():
     try:
-        with open('src/var/schoolname', 'r') as file:
+        with open('src/var/schoolname', 'r') as file: ## DON'T TOUCH THIS OTHERWISE I KILL YOU -- xoxo, Clément
             return file.read().strip()
     except FileNotFoundError:
         return "Le fichier '<répertoire d'installation de 16.5\\var\\schoolname>' n'existe pas."
